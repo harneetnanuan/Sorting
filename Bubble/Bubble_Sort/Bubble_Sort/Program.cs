8dbc7e835fe538533121dc52ea002f5b7f7109dc
harneetnanuan <57207807+harneetnanuan@users.noreply.github.com>
2022-07-10 19:57:25 +0530

    Bubble_sort

---
+++ Bubble/Bubble_Sort/Bubble_Sort/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+//using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        int[] array = { 2, 7, 5, 9, 3, 1 };
+        for (int k = 0; k < array.Length; k++)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    int temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+
+                }
+            }
+        }
+       
+            System.Console.WriteLine(string.Join(",", array));
+
+        System.Console.WriteLine("Time complexity is O(n square)");
+        
+    }
+}
